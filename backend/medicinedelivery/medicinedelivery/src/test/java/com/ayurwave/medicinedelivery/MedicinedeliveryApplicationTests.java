d909375eebbb778c937b61c7c41b06ace26341d6
Adil <adil.amin820@gmail.com>
2024-01-27 21:36:02 +0530

    Setting up the Backend (Spring Boot)
    Dependencies Added -
    
    1. Spring Web WEB
    2. Spring Data JPA SQL
    3. Lombok DEVELOPER TOOLS

---
+++ backend/medicinedelivery/medicinedelivery/src/test/java/com/ayurwave/medicinedelivery/MedicinedeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayurwave.medicinedelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicinedeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
